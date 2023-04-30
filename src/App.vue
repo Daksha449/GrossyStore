17282de2387f228066448284e28bd3d8de673194
Daksha Chava <daksha@Dakshas-MacBook-Air.local>
2023-04-30 16:16:55 -0400

    First Commit =

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyHeader Name="Grossy"/>
+  <ProductBox :store=store  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyHeader from './components/MyHeader.vue'
+import ProductBox from  './components/ProductBox.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // Step 2 register the component 
+    MyHeader,
+    ProductBox
+  },
+
+  data(){
+    return {
+      store: []
+    }
+  },
+  methods: {
+    // promises
+
+      async fetchproducts(){
+         const res= await fetch('http://localhost:8888/store')
+         const data= await res.json()
+          console.log(data)
+         return data
+      }
+
+  },
+
+  async created(){
+
+    this.store = await this.fetchproducts()
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
+
+body {
+  font-family: 'Montserrat', sans-serif;
+  background-color: honeydew;
+  
+}
+.container {
+  max-width: 40px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 0.3em solid black;
+  padding: 30px;
+  border-radius: 5px;
+}
+
+div{
+  margin-bottom: 0.5em;
+
+}
+
+
 </style>
